831f9136a9676e0697959f27629ab8222cc4adcd
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2022-05-15 17:00:00 +0100

    Edited clue.js file

--- src/clue.js
+++ src/clue.js
@@ -15,12 +15,20 @@ image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg", color: "b
 
 // Rooms Array
 
-const roomsArray = ["Dining Room",
-"Conservatory", "Kitchen", "Study", "Library", "Billiard Room", "Lounge", "Ballroom", "Hall", "Spa", "Living Room", "Observatory", "Theater", "Guest House", "Patio"];
+const roomsArray = [{name: "Dining Room",
+name: "Conservatory", name: "Kitchen", name: "Study", name: "Library", name: "Billiard Room", name:"Lounge", name: "Ballroom", name: "Hall", name: "Spa", name: "Living Room", name: "Observatory", name: "Theater", name: "Guest House", name: "Patio"}];
 
 // Weapons Array
 
-const weaponsArray = ["rope", "knife", "candlestick", "dumbbell", "poison", "axe", "bat", "trophy", "pistol"];
+const weaponsArray = [{name: "rope", weight: 10},
+  {name: "knife", weight: 8},
+  {name: "candlestick", weight: 2},
+  {name: "dumbbell",  weight: 30}, 
+  {name: "poison", weight: 2},
+  {name: "axe", weight: 15},
+  {name: "bat", weight: 13}, 
+  {name: "trophy", weight: 25},
+  {name: "pistol", weight: 20}];
 
 
 // ITERATION 2
